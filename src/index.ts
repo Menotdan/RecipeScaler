d4aac777ae6b2bcfab91ce7438e080fe995e9317
Menotdan <32620310+Menotdan@users.noreply.github.com>
2023-09-02 15:47:50 -0400

    Add testing code for the scale button.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi");
+function scaleClicked(event: MouseEvent) {
+    console.log("weeee");
+}
+
+var scaleButton = document.getElementById("scale-button");
+if (scaleButton == null) {
+    console.error("Scale button not found.");
+}
+else {
+    scaleButton.onclick = scaleClicked;
+}
